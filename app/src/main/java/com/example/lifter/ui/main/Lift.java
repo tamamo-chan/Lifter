9cef1fa184265c195e46f627ff16ab9c836400ee
Tama-chan <lolipirate1@gmail.com>
2020-08-15 23:39:30 +0200

    Added comments and removed redundant code.

--- app/src/main/java/com/example/lifter/ui/main/Lift.java
+++ app/src/main/java/com/example/lifter/ui/main/Lift.java
@@ -34,47 +34,8 @@ import java.util.Locale;
 
 public class Lift extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-    SoundPool sp;
-    Context context;
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
     public Lift() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Frag1.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Lift newInstance(String param1, String param2) {
-        Lift fragment = new Lift();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        Log.d("lift","onCreate invoked");
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Nullable
@@ -94,11 +55,15 @@ public class Lift extends Fragment {
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(),
                 R.layout.support_simple_spinner_dropdown_item, items);
 
+        // Add the spinner/dropdown menu.
         menu.setAdapter(adapter);
 
+        // Based on the day, different workout routines are required. The respective days are
+        // chosen in the spinner
         menu.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
-            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long l) {
+            public void onItemSelected(AdapterView<?> adapterView, View view, int position,
+                                       long l) {
                 switch (position) {
                     case 0: {
                         TextView text = lift.findViewById(R.id.workout_name1);
@@ -157,6 +122,7 @@ public class Lift extends Fragment {
 
                 final Button button = lift.findViewById(R.id.button);
 
+                // Set a listener for the save button
                 button.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
@@ -176,11 +142,22 @@ public class Lift extends Fragment {
 
     public void buttonResponse(View view) {
 
+        // Saving will make a file named after the date of the day.
         SimpleDateFormat formatter = new SimpleDateFormat(("dd_MM_yyyy"), Locale.ENGLISH);
         Date now = new Date();
         String filename = formatter.format(now)+ ".txt";
 
-        File file = new File(getContext().getFilesDir().getPath().toString(), filename);
+        File file = new File(getContext().getFilesDir().getPath(), filename);
+
+        /*
+        The save file will have the following format:
+        Day selected in the spinner
+        Exercise 1, circle 1 result - E1, c2 - ... - E1, c8 - Exercise 1, weight
+        Exercise 2, circle 1 result - ...
+        ...
+        Exercise 5, circle 1 result - ...
+
+         */
 
         try {
 
